da7e0194e117aab6c40d6eea14b95bdd5099ccf5
kazuya-umeki <kazuyaumeki1115@gmail.com>
2024-01-06 01:45:36 +0900

    Set reset css.

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,15 +1,28 @@
+import styled from 'styled-components'
+
 type MainLayoutProps = {
   children: React.ReactNode
 }
 
 export default function MainLayout({ children }: MainLayoutProps): JSX.Element {
   return (
-    <main id="main" className="h-screen flex overflow-hidden bg-red-50">
-      <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <div className="flex-1 relative overflow-y-auto focus:outline-none">
-          {children}
-        </div>
-      </div>
-    </main>
+    <App>
+      <Wrapper>{children}</Wrapper>
+    </App>
   )
 }
+
+const App = styled.div`
+  overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  margin: 0px auto;
+  max-width: 120ch;
+  padding: 5rem 3.33333rem 0px;
+
+  /* media queries are no problem */
+  @media (max-width: 62.5em) {
+    padding: 5.55556rem 2rem 1.66667rem;
+  }
+`
