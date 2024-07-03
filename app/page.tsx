27b14767cfaa4e03e643462078c9c6704303ce04
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-07-03 21:47:44 +0900

    Add server-side data fetching with delay for feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-import { Feed as FeedType } from "@/model/feed.model";
-import { fetchData } from "@/utils/api";
+import { Suspense } from "react";
+import Contact from "@/features/subcontent/contact";
+import Empty from "@/features/subcontent/empty";
+import Feed from "@/features/subcontent/feed";
 import Profile from "@/features/front/profile";
 import style from "@/components/tmp.module.css";
-import Feed from "@/features/subcontent/feed";
-import Contact from "@/features/subcontent/contact";
 
 export default async function Page() {
-  const feed = await loader();
-
   return (
     <div className={style.container}>
       <Profile />
-      {feed && <Feed feed={feed} />}
+      <Suspense fallback={<Empty label="Qiita" />}>
+        <Feed />
+      </Suspense>
       <Contact />
     </div>
   );
-}
-
-async function loader() {
-  const ENDPOINT = `${process.env.NEXT_DRUPAL_API}/api/qiita/feed/${process.env.NEXT_PUBLIC_QITIA_ID}`;
-  const feed = await fetchData<FeedType>(ENDPOINT);
-  return feed;
 }
