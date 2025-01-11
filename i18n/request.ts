0af0f51a8399bf2d77fbaf1ddf9084956ebfc85e
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2025-01-12 07:49:24 +0900

    Update next-intl

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,17 @@
 import { getRequestConfig } from "next-intl/server";
-import { routing } from "./i18n/routing";
+import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
-  // Ensure that the incoming locale is valid
+
+  // Ensure that a valid locale is used
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
   }
+
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
-import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'ja'],
- 
-  // Used when no locale matches
-  defaultLocale: 'ja'
-});
- 
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+
+export default createMiddleware(routing);
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ja|en)/:path*']
+  matcher: ["/", "/(ja|en)/:path*"],
 };
